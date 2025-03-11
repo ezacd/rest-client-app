7fde35c9543f2040fe70531ada80a69d0ed6ac72
ezacd <yaroslav.us2312@gmail.com>
2025-03-11 17:30:21 +0300

    refactor: requestService.ts

--- postman/src/services/requestService.ts
+++ postman/src/services/requestService.ts
@@ -1,53 +1,33 @@
 import apiClient from './api';
 
 type Param = { [k: string]: string };
+type Headers = Record<string, string>;
 
-export const getData = async (url: string, headers: Record<string, string>) => {
+export const getData = async (url: string, headers: Headers) => {
   console.log(headers);
   return apiClient.get(url, { headers });
 };
 
-export const postData = async (
-  url: string,
-  data: Param,
-  headers: Record<string, string>,
-) => {
+export const postData = async (url: string, data: Param, headers: Headers) => {
   return apiClient.post(url, data, { headers });
 };
 
-export const putData = async (
-  url: string,
-  data: Param,
-  headers: Record<string, string>,
-) => {
+export const putData = async (url: string, data: Param, headers: Headers) => {
   return apiClient.put(url, data, { headers });
 };
 
-export const deleteData = async (
-  url: string,
-  headers: Record<string, string>,
-) => {
+export const deleteData = async (url: string, headers: Headers) => {
   return apiClient.delete(url, { headers });
 };
 
-export const patchData = async (
-  url: string,
-  data: Param,
-  headers: Record<string, string>,
-) => {
+export const patchData = async (url: string, data: Param, headers: Headers) => {
   return apiClient.patch(url, data, { headers });
 };
 
-export const headData = async (
-  url: string,
-  headers: Record<string, string>,
-) => {
+export const headData = async (url: string, headers: Headers) => {
   return apiClient.head(url, { headers });
 };
 
-export const optionsData = async (
-  url: string,
-  headers: Record<string, string>,
-) => {
+export const optionsData = async (url: string, headers: Headers) => {
   return apiClient.options(url, { headers });
 };
