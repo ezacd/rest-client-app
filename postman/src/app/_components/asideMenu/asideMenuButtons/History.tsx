2e8b22a2638c8128b2bb9e6ede9b5ea80b0cd1f2
ezacd <yaroslav.us2312@gmail.com>
2025-03-13 19:16:58 +0300

    feat: history menu desine

--- postman/src/app/_components/asideMenu/asideMenuButtons/History.tsx
+++ postman/src/app/_components/asideMenu/asideMenuButtons/History.tsx
@@ -2,6 +2,8 @@ import HistorySvg from '@/assets/icons/history.svg';
 import styles from '@/app/_components/asideMenu/asideMenuButtons/History.module.css';
 import { useTranslations } from 'next-intl';
 import CloseSvg from '@/assets/icons/close.svg';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/app/_store/store';
 
 type BurgerMenuState = {
   isOpen: boolean;
@@ -38,6 +40,8 @@ export default function History({ burgerMenu, setBurgerMenu }: HistoryProps) {
 }
 
 export function HistoryAsideMenu({ setBurgerMenu }: setHistoryProps) {
+  const history = useSelector((state: RootState) => state.request.history);
+
   const handleClick = () => {
     setBurgerMenu({ isOpen: false, menuType: 'history' });
   };
@@ -47,8 +51,19 @@ export function HistoryAsideMenu({ setBurgerMenu }: setHistoryProps) {
       <div className={styles.closeSvgBox} onClick={handleClick}>
         <CloseSvg className={styles.closeSvg} />
       </div>
+      <h2 className={styles.historyH2}>History</h2>
+
       <ul className={styles.historyAsideMenuUl}>
-        <li>history</li>
+        {history.map((item, index) => (
+          <li key={index} className={styles.historyAsideMenuLi}>
+            <p className={styles.historyAsideMenuLiMethod}>
+              {item.request.http_method}
+            </p>
+            <p className={styles.historyAsideMenuLiRequest}>
+              {item.request.url}
+            </p>
+          </li>
+        ))}
       </ul>
     </div>
   );
