2ae210f54237b8727734956f5d28abdfcadec595
ezacd <yaroslav.us2312@gmail.com>
2025-03-28 17:08:45 +0300

    fix: middleware i18n login

--- postman/src/middleware.ts
+++ postman/src/middleware.ts
@@ -9,17 +9,32 @@ export function middleware(req: NextRequest) {
 
   const token = req.cookies.get('token')?.value;
   const currentPath = req.nextUrl.pathname;
-  const langPrefix = currentPath.endsWith('/ru') ? '/ru' : '/en';
 
   const isRegisterPage = /^\/(ru|en)?\/?register$/.test(currentPath);
   const isLoginPage = /^\/(ru|en)?\/?login$/.test(currentPath);
 
+  const referer = req.headers.get('referer')?.split('/');
+
+  const last = referer![referer!.length - 1] || 'register';
+
+  console.log('++++++++++++++++++++++++++++++++++++++++++++++');
+
   if (!token && !isRegisterPage && !isLoginPage) {
-    return NextResponse.redirect(new URL(`${langPrefix}/register`, req.url));
+    const localeMatches = [...currentPath.matchAll(/\/(ru|en)/g)];
+
+    const lang = localeMatches.length
+      ? localeMatches[localeMatches.length - 1][1]
+      : 'en';
+
+    if (last === 'register') {
+      return NextResponse.redirect(new URL(`/${lang}/register`, req.url));
+    } else {
+      return NextResponse.redirect(new URL(`/${lang}/login`, req.url));
+    }
   }
 
-  if (token && currentPath !== `${langPrefix}`) {
-    return NextResponse.redirect(new URL(`${langPrefix}/`, req.url));
+  if (token) {
+    return NextResponse.redirect(new URL(`/`, req.url));
   }
 
   return response;
